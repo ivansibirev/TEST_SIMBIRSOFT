c9404a9b76e1d25a833177ebab72fdef469c6e27
ivansibirev <ivansibirevmail@gmail.com>
2016-05-31 22:27:22 +0300

    DZ1_commit_2
    Добавление файлов

---
+++ T/C_W.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T
+{
+    /// <summary>  Cтатичный класс, в котором реализован вывод цветного текста в консоль. Этот класс был обрезан и взят из другого приложения.</summary>
+    public partial class C
+    {
+        static private class FC
+        {
+            static private void Do(ConsoleColor MyConsoleColor) { Console.ForegroundColor = MyConsoleColor; }
+            public static void Black() { Do(ConsoleColor.Black); }
+            public static void Blue() { Do(ConsoleColor.Blue); }
+            public static void Cyan() { Do(ConsoleColor.Cyan); }
+            public static void DarkBlue() { Do(ConsoleColor.DarkBlue); }
+            public static void DarkCyan() { Do(ConsoleColor.DarkCyan); }
+            public static void DarkGray() { Do(ConsoleColor.DarkGray); }
+            public static void DarkGreen() { Do(ConsoleColor.DarkGreen); }
+            public static void DarkMagenta() { Do(ConsoleColor.DarkMagenta); }
+            public static void DarkRed() { Do(ConsoleColor.DarkRed); }
+            public static void DarkYellow() { Do(ConsoleColor.DarkYellow); }
+            public static void Gray() { Do(ConsoleColor.Gray); }
+            public static void Green() { Do(ConsoleColor.Green); }
+            public static void Magenta() { Do(ConsoleColor.Magenta); }
+            public static void Red() { Do(ConsoleColor.Red); }
+            public static void White() { Do(ConsoleColor.White); }
+            public static void Yellow() { Do(ConsoleColor.Yellow); }
+            public static void Standart() { Do(ConsoleColor.Gray); }
+        }
+        /// <summary>Write</summary>
+        static public class W
+        {
+            static private void Do(string STR) { Console.Write(STR); C.FC.Standart(); }
+            public static void Black(string STR) { C.FC.Black(); Do(STR); }
+            public static void Blue(string STR) { C.FC.Blue(); Do(STR); }
+            public static void Cyan(string STR) { C.FC.Cyan(); Do(STR); }
+            public static void DarkBlue(string STR) { C.FC.DarkBlue(); Do(STR); }
+            public static void DarkCyan(string STR) { C.FC.DarkCyan(); Do(STR); }
+            public static void DarkGray(string STR) { C.FC.DarkGray(); Do(STR); }
+            public static void DarkGreen(string STR) { C.FC.DarkGreen(); Do(STR); }
+            public static void DarkMagenta(string STR) { C.FC.DarkMagenta(); Do(STR); }
+            public static void DarkRed(string STR) { C.FC.DarkRed(); Do(STR); }
+            public static void DarkYellow(string STR) { C.FC.DarkYellow(); Do(STR); }
+            public static void Gray(string STR) { C.FC.Gray(); Do(STR); }
+            public static void Green(string STR) { C.FC.Green(); Do(STR); }
+            public static void Magenta(string STR) { C.FC.Magenta(); Do(STR); }
+            public static void Red(string STR) { C.FC.Red(); Do(STR); }
+            public static void White(string STR) { C.FC.White(); Do(STR); }
+            public static void Yellow(string STR) { C.FC.Yellow(); Do(STR); }
+            public static void Standart(string STR) { C.FC.Standart(); Do(STR); }
+            public static void n() { C.FC.Standart(); Do("\n"); }
+            public static void n(int n) { C.FC.Standart(); for (int i = 0; i < n; i++) Do("\n"); }
+            public static void SeeAllColors()
+            {
+                Standart("Black");
+                Black("Black\n");
+                Standart("Blue");
+                Blue("Blue\n");
+                Standart("Cyan");
+                Cyan("Cyan\n");
+                Standart("DarkBlue");
+                DarkBlue("DarkBlue\n");
+                Standart("DarkCyan");
+                DarkCyan("DarkCyan\n");
+                Standart("DarkGray");
+                DarkGray("DarkGray\n");
+                Standart("DarkGreen");
+                DarkGreen("DarkGreen\n");
+                Standart("DarkMagenta");
+                DarkMagenta("DarkMagenta\n");
+                Standart("DarkRed");
+                DarkRed("DarkRed\n");
+                Standart("DarkYellow");
+                DarkYellow("DarkYellow\n");
+                Standart("Gray");
+                Gray("Gray\n");
+                Standart("Green");
+                Green("Green\n");
+                Standart("Magenta");
+                Magenta("Magenta\n");
+                Standart("Red");
+                Red("Red\n");
+                Standart("White");
+                White("White\n");
+                Standart("Yellow");
+                Yellow("Yellow\n");
+                Standart("Standart");
+                Standart("Standart\n");
+            }
+        }
+    }
+}
---
+++ T/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T
+{
+    class Program
+    {
+        static void Main(string[] args){
+            DoIt("TheTextFile.txt", "Dictionary.txt",10);            
+            Console.Read();
+        }
+        /// <summary>Метод, готовый к использованию.</summary>
+        /// <param name="TextFilePath">Путь к тексту</param>
+        /// <param name="DictionaryFilePath">Путь к словарю.</param>
+        /// <param name="MaxLineCount">Максимум предложений в HTMl файле</param>
+        static void DoIt(string TextFilePath, string DictionaryFilePath, int MaxLineCount)
+        {
+            DictionaryClass myDictionary = new DictionaryClass(DictionaryFilePath);
+            try {
+                System.IO.StreamReader sr = System.IO.File.OpenText(TextFilePath);
+                sr.Close();
+            }
+            catch {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(System.IO.File.Open(TextFilePath, System.IO.FileMode.Create));
+                sw.Close();
+            }
+            using (System.IO.StreamReader sr = System.IO.File.OpenText(TextFilePath))
+            {
+                string s = "";//Для чтения из TextFilePath
+                string StringOverview = "";//Строка "обзора конца предложения".
+                string a = "";//Вспомогательная переменная
+                HTMLConnectionClass HTMLConnection = new HTMLConnectionClass(MaxLineCount, 1);
+                Func<string, object> Writer = (string w) =>
+                {   //Лямбда-выражение или функция централизованного вывода (записи) текста на экран, в HTML файл...
+                    if (HTMLConnection.counter > HTMLConnection.maxcount)
+                    {   //Смена HTML файла
+                        HTMLConnection.fin();
+                        HTMLConnectionClass newConnection = new HTMLConnectionClass(HTMLConnection.maxcount, HTMLConnection.id + 1);
+                        HTMLConnection = newConnection;
+                    }
+                    foreach (string str in a.Split(' '))
+                    {   //Пишем текст
+                        if (myDictionary.Contains_(str))
+                        {   //Проверка, есть ли в словаре слово.
+                            C.W.Cyan(str + " ");//Пишем на экран в цвере.
+                            HTMLConnection.WriteLine("<i><b>" + str + "</b></i>");//Пишем в HTML жирным курсивом.
+                        }
+                        else
+                        {
+                            C.W.Gray(str + " ");
+                            HTMLConnection.WriteLine(str);
+                        }
+                    }
+                    C.W.n();
+                    HTMLConnection.WriteLine("<br>");
+                    HTMLConnection.counter++;
+                    return new object();
+                };
+                while ((s = sr.ReadLine()) != null)
+                {   //Читаем из исходного файла текст
+                    StringOverview = StringOverview + " " + s;
+                    if (StringOverview.Contains(". "))
+                    {//Отслеживаем наличие конца предложения и переходим к действиям
+                        a = StringOverview.Substring(0, StringOverview.IndexOf(". ") + 1);
+                        Writer(a);
+                        StringOverview = StringOverview.Substring(StringOverview.IndexOf(". ") + 1);
+                    }
+                }
+                a = StringOverview;
+                Writer(a);
+                HTMLConnection.fin();
+                sr.Close();
+            }
+        }
+    }
+    
+
+}
